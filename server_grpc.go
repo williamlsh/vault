888d8cde39d161cb4448594e454b8ccc7f8522ba
Willaim <1340440725fhs@gmail.com>
2019-02-24 22:58:43 +0800

    Update server_grpc.go

--- server_grpc.go
+++ server_grpc.go
@@ -26,3 +26,46 @@ func (s *grpcServer) Validate(ctx context.Context, r *pb.ValidateRequest) (*pb.V
 	}
 	return resp.(*pb.ValidateResponse), nil
 }
+
+// EncodeGRPCHashRequest is an EncodeRequestFunc type function defined by Go
+// kit, it's used to translate our own hashRequest type into a protocol buffer
+// type.
+func EncodeGRPCHashRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(hashRequest)
+	return &pb.HashRequest{Password: req.Password}, nil
+}
+
+func DecodeGRPCHashRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(*pb.HashRequest)
+	return hashRequest{Password: req.Password}, nil
+}
+
+func EncodeGRPCHashResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(hashResponse)
+	return &pb.HashResponse{Hash: res.Hash, Err: res.Err}, nil
+}
+
+func DecodeGRPCHashResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(*pb.HashResponse)
+	return hashResponse{Hash: res.Hash, Err: res.Err}, nil
+}
+
+func EncodeGRPCValidateRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(validateRequest)
+	return &pb.ValidateRequest{Password: req.Password, Hash: req.Hash}, nil
+}
+
+func DecodeGRPCValidateRequest(ctx context.Context, r interface{}) (interface{}, error) {
+	req := r.(*pb.ValidateRequest)
+	return validateRequest{Password: req.Password, Hash: req.Hash}, nil
+}
+
+func EncodeGRPCValidateResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(validateResponse)
+	return &pb.ValidateResponse{Valid: res.Valid}, nil
+}
+
+func DecodeGRPCValidateResponse(ctx context.Context, r interface{}) (interface{}, error) {
+	res := r.(*pb.ValidateResponse)
+	return validateResponse{Valid: res.Valid}, nil
+}
