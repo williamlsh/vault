4411e3b22b4b4a525556b65cdecf2448ac68daa7
Willaim <1340440725fhs@gmail.com>
2019-02-24 22:16:09 +0800

    implements grpcServer with contains two handlers of endpoint

---
+++ server_grpc.go
@@ -0,0 +1,28 @@
+package vault
+
+import (
+	"context"
+	"vault/pb"
+
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+)
+
+type grpcServer struct {
+	hash, validate grpctransport.Handler
+}
+
+func (s *grpcServer) Hash(ctx context.Context, r *pb.HashRequest) (*pb.HashResponse, error) {
+	_, resp, err := s.hash.ServeGRPC(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*pb.HashResponse), nil
+}
+
+func (s *grpcServer) Validate(ctx context.Context, r *pb.ValidateRequest) (*pb.ValidateResponse, error) {
+	_, resp, err := s.validate.ServeGRPC(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*pb.ValidateResponse), nil
+}
