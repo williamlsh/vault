a45873ae1f01f9bfc8d36c5b7950d76b43eb376b
Willaim <1340440725fhs@gmail.com>
2019-02-25 15:01:45 +0800

    Create gRPC client to intereact with Vault service

---
+++ client/grpc/client.go
@@ -0,0 +1,30 @@
+package grpc
+
+import (
+	"vault"
+	"vault/pb"
+
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+	"google.golang.org/grpc"
+)
+
+// New returns a gRPC client to intereact with Vault service.
+func New(conn *grpc.ClientConn) vault.Service {
+	var hashEndpoint = grpctransport.NewClient(
+		conn, "Vault", "Hash",
+		vault.EncodeGRPCHashRequest,
+		vault.DecodeGRPCHashResponse,
+		pb.HashResponse{},
+	).Endpoint()
+	var validateEndpoint = grpctransport.NewClient(
+		conn, "Vault", "Validate",
+		vault.EncodeGRPCValidateRequest,
+		vault.DecodeGRPCValidateResponse,
+		pb.ValidateResponse{},
+	).Endpoint()
+
+	return vault.Endpoints{
+		HashEndpoint:     hashEndpoint,
+		ValidateEndpoint: validateEndpoint,
+	}
+}
