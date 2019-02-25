e58bcd3a858b42c8f7b06542194e45eacd65d829
Willaim <1340440725fhs@gmail.com>
2019-02-25 13:32:19 +0800

    basic vaultd with gRPC transport

---
+++ cmd/vaultd/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+
+	"vault"
+	"vault/pb"
+)
+
+func main() {
+	var (
+		httpAddr = flag.String("http", ":8080", "http listen address")
+		gRPCAddr = flag.String("grpc", ":8081", "gRPC isten address")
+	)
+	flag.Parse()
+
+	ctx := context.Background()
+	srv := vault.NewService()
+	errChan := make(chan error)
+
+	// Traps termination signal (such as ctrl + C) and sends an error down
+	// errChain.
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	hashEndpoint := vault.MakeHashEndpoint(srv)
+	validateEndpoint := vault.MakeValidateEndpoint(srv)
+	endpoints := vault.Endpoints{
+		HashEndpoint:     hashEndpoint,
+		ValidateEndpoint: validateEndpoint,
+	}
+
+	// gRPC transport
+	go func() {
+		lis, err := net.Listen("tcp", *gRPCAddr)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		log.Println("grpc:", *gRPCAddr)
+		handler := vault.NewGRPCServer(ctx, endpoints)
+		gRPCServer := grpc.NewServer()
+		pb.RegisterVaultServer(gRPCServer, handler)
+		errChan <- gRPCServer.Serve(lis)
+	}()
+
+	log.Fatalln(<-errChan)
+}
