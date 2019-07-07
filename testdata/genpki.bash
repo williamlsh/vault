7f2af30da82cb4cc381b419c0136f4b9189c6613
William <webdevelopper@protonmail.ch>
2019-07-07 20:08:55 +0800

    TLS: generate TLS credentials(#5)

---
+++ testdata/genpki.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Generate a 2048-bit RSA private key.
+openssl genrsa -out server-key.pem 2048
+
+# Generate a CSR for a private key.
+openssl req -new -sha256 -key server-key.pem -out server-csr.pem
+
+# Create a self-signed certificate.
+openssl x509 -req -in server-csr.pem -signkey server-key.pem -out server-cert.pem
