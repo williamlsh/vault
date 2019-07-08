4d319c9b8d43afa0e392334273aaf373541c970c
William <webdevelopper@protonmail.ch>
2019-07-08 14:38:03 +0800

    Delete run.bash

--- cmd/vaultd/run.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export VAULTD_LOG_LEVEL=all
-cd $(go list -f '{{.Dir}}' github.com/williamzion/vault/cmd/vaultd) && go run \
-  -race . \
-  -http-addr=":8080" \
-  -grpc-addr=":8081"
