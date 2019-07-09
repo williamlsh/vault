51a9f0c3ea8bab1fb4d33df978cfa4e569559e05
William <webdevelopper@protonmail.ch>
2019-07-09 20:04:19 +0800

    endpoint-middleware: implement endpoint with logger and prometheus metrics(#11)

--- pkg/vaultservice/middleware.go
+++ pkg/vaultservice/middleware.go
@@ -28,14 +28,14 @@ type loggingMiddleware struct {
 
 func (mw loggingMiddleware) Hash(ctx context.Context, password string) (hash string, err error) {
 	defer func() {
-		mw.logger.Log("method", "hash", "password", password, "hash", hash, "token", ctx.Value(jwt.JWTTokenContextKey).(string), "err", err)
+		mw.logger.Log("method", "Hash", "password", password, "hash", hash, "token", ctx.Value(jwt.JWTTokenContextKey).(string), "err", err)
 	}()
 	return mw.next.Hash(ctx, password)
 }
 
 func (mw loggingMiddleware) Validate(ctx context.Context, password, hash string) (v bool, err error) {
 	defer func() {
-		mw.logger.Log("method", "validate", "password", password, "hash", hash, "valid", v, "token", ctx.Value(jwt.JWTTokenContextKey).(string), "err", err)
+		mw.logger.Log("method", "Validate", "password", password, "hash", hash, "valid", v, "token", ctx.Value(jwt.JWTTokenContextKey).(string), "err", err)
 	}()
 	return mw.next.Validate(ctx, password, hash)
 }
